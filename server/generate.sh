094d735969abf6b5ee8d827e401852ebaca8faa8
zhengdechang <2473023641@qq.com>
2023-09-10 15:59:48 +0000

    fix generate fail

--- server/generate.sh
+++ server/generate.sh
@@ -5,6 +5,6 @@
 
 mkdir -p output
 
-protoc --python_out=output --grpc_python_out=output ./proto/simple_grpc.proto
+python3  -m grpc_tools.protoc --proto_path=./proto --python_out=./output --grpc_python_out=./output simple_grpc.proto
 
 echo "Generation complete! The output files are located in the Output folder."
