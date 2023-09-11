64c0c0745e86160933fd2e5807f9e30d0a152ea7
zhengdechang <2473023641@qq.com>
2023-09-11 16:24:05 +0800

    update

--- .history/server/generate_20230911161144.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# If requirement is not installed, open the following comment
-# chmod +x ./activate && ./activate
-
-python3  -m grpc_tools.protoc --proto_path=./proto --python_out=./proto --grpc_python_out=./proto simple_grpc.proto
-
-echo "Generation complete! The output files are located in the proto folder."
