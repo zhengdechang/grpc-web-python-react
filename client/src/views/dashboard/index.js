29f42bf9c926fca591a3cd32414656a4f6699449
zhengdechang <2473023641@qq.com>
2023-09-11 18:03:57 +0800

    update generate grpc-web config

--- client/src/views/dashboard/index.js
+++ client/src/views/dashboard/index.js
@@ -1,7 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react';
+import { Button, Input, Typography } from 'antd';
+import { HelloReq } from '@/grpc-api/simple_grpc_pb.js';
+import { SimpleGrpcClient } from '@/grpc-api/simple_grpc_grpc_web_pb.js';
 
-function Dashboard() {
-  return <div>{'dashboard'}</div>
-}
+const { Text } = Typography;
 
-export default Dashboard
+const IndexPage = () => {
+  const [username, setUsername] = useState('');
+  const [grpcResponse, setGrpcResponse] = useState('');
+
+  const client = new SimpleGrpcClient('http://localhost:50051', null, null);
+
+  const onSubmit = () => {
+    let req = new HelloReq();
+    req.setName(username);
+
+    client.sayHello(req, {}, (err, res) => {
+      if (err) {
+        console.log(err);
+        setGrpcResponse(JSON.stringify(err));
+        return;
+      }
+      setGrpcResponse(JSON.stringify(res.toObject()));
+    });
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Input
+        placeholder="Name"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Button type="primary" onClick={onSubmit}>
+        Click Me!
+      </Button>
+      <Text type="secondary" style={{ maxWidth: '400px', wordWrap: 'break-word' }}>
+        {grpcResponse}
+      </Text>
+    </div>
+  );
+};
+
+export default IndexPage;
