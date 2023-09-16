a50f1240d6f558720840cb9a63a5c724fc4aaaab
zhengdechang <2473023641@qq.com>
2023-09-17 01:37:51 +0800

    update gui control

--- client/src/utils/get-grpc-url.js
+++ client/src/utils/get-grpc-url.js
@@ -1,5 +1,5 @@
 const getGrpcUrl = () => {
-    return process.env.REACT_APP_GRPC_URL || 'https://d0a9-206-190-239-91.ngrok-free.app'
+    return process.env.REACT_APP_GRPC_URL || 'https://127.0.0.1:5000'
 }
 
 
